5c4388254443a13c2eec0431e4606eabc3e82bf1
Chris Martyniuk <cmartyniuk1@gmail.com>
2018-02-15 20:05:19 -0500

    Fixed pathing issue between brackets and github testing

--- js/login.js
+++ js/login.js
@@ -1,8 +1,24 @@
+var path;
+determinePath();
 
 //TODO ------------------------------ Add login validation
 
 $("#login-button").click(function() {
-   $(location).attr('href','/pathfound/gameroom.html');
+   $(location).attr('href', path + "gameroom.html");
 });
 
 //TODO END -----------------------------------------------
+
+function determinePath() {
+    
+    if (location.hostname == "cmartyniuk1656.github.io")
+        
+        path = "pathfound/";
+    
+    else {
+        path="";
+    }
+    
+    return path;
+        
+};
