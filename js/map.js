4c6db6d46a3644b5b8852c0795711e0ebffe5633
Collin Livingstone <collinlivingstone@gmail.com>
2018-03-17 16:56:06 -0400

    Updates to map JS

--- js/map.js
+++ js/map.js
@@ -1,28 +1,57 @@
-var scale = 0;
-var map = document.getElementById("map");
+//Set document and location to current window / variable declaration
+    var scale = 0,
+    map = document.getElementById("map");
 
-function zoomInMap(){
-   console.info(scale);
-   scale++;
-   map.style.transform = "scale(1."+ scale +")";
-   console.info("clicked");
-}
-
-function zoomOutMap(){
-   console.info(scale);
-   scale--;
-   map.style.transform = "scale(1."+ scale +")";
+//Map Controller Object
+var mapController = {
+    
+    //Keep track of all the map objects in the gameroom
+    "mapList": {},
+    
+    
+    //Utility functions for map controls
+    "controls": {
+        
+        //Zoom in the map
+        "zoomInMap": function() {
+            console.info(scale);
+            scale++;
+            map.style.transform = "scale(1."+ scale +")";
+        },
+        
+        //Zoom out the map
+        "zoomOutMap": function() {
+            console.info(scale);
+            scale--;
+            map.style.transform = "scale(1."+ scale +")";
+        }   
+    },
+    
+    //Event handlers for map controls
+    "events": {
+        
+        "zoomInMap": $("#zoom-in-btn").click(function() {
+                mapController.controls.zoomInMap();
+            }),
+        
+        "zoomOutMap": $("#zoom-out-btn").click(function() {
+                mapController.controls.zoomOutMap();
+            })
+    },
+    
+    "util": {
+        
+        "addEventListeners": function() {
+            
+            this.events.zoomInMap();
+            this.events.zoomOutMap();
+            
+        }
+    }
 }
 
 $(document).ready(function() {
     
-    $("#zoom-in-btn").click(function() {
-        zoomInMap();
-    });
-
-$("#zoom-out-btn").click(function() {
-        zoomOutMap();
-    });
+    mapController.util.addEventListeners();
     
 })
-
