6ca353df178f1566561657a9fca7cb56d818dc63
Chris Martyniuk <cmartyniuk1@gmail.com>
2018-03-18 00:14:35 -0400

    Added pan left/right functionality

--- js/map.js
+++ js/map.js
@@ -4,15 +4,22 @@ var MapController = {
     //Keep track of all the map objects in the gameroom
     "mapList": {},
     
+    //Used for zoom logic
     "zoomLevels": ['0.25', '0.50', '0.75', '1.0', '1.25', '1.50', '1.75', '2.0'],
     "selectedZoomIndex": 3,
+    
+    //Used for pan logic
+    "leftMargin": 0,
+    "rightMargin": 0,
+    "topMargin": 0,
+    "bottomMargin": 0,
     
     "map": document.getElementById('map'),
     "mapGrid": document.getElementById('map-grid'),
 
     
-    "mapCellHeight": 21,                                    //TODO: Make user selectable
-    "mapCellWidth": 21,                                     //TODO: Make user selectable
+    "mapCellHeight": 21,                            //TODO: Make user selectable
+    "mapCellWidth": 21,                             //TODO: Make user selectable
     
     
     "backgroundImg": '', 
@@ -39,7 +46,51 @@ var MapController = {
                 MapController.selectedZoomIndex --;
                 MapController.map.style.transform = 'scale(' + MapController.zoomLevels[MapController.selectedZoomIndex] + ')';
             }
-        }   
+        },
+        
+        "panLeft": function() {
+            
+            var styleString;
+            
+            MapController.leftMargin += 50;
+            
+            styleString = MapController.leftMargin.toString() + 'px';
+            MapController.map.style.marginLeft = styleString;
+        
+        },
+        
+        "panRight": function() {
+            
+            var styleString;
+            
+            MapController.leftMargin -= 50;
+            
+            styleString = MapController.leftMargin.toString() + 'px';
+            MapController.map.style.marginLeft = styleString;
+                     
+        },
+        
+        "panUp": function() {
+            
+//            var styleString;
+//            
+//            MapController.bottomMargin += 50;
+//            
+//            styleString = MapController.bottomMargin.toString() + 'px';
+//            MapController.map.style.marginBottom = styleString;
+            
+        },
+        
+        "panDown": function() {
+            
+//            var styleString;
+//            
+//            MapController.topMargin -= 50;
+//            
+//            styleString = MapController.topMargin.toString() + 'px';
+//            MapController.map.style.marginTop = styleString;
+            
+        }
     },
     
     //Event handlers for map controls
@@ -55,6 +106,26 @@ var MapController = {
             //Zoom out event handler
             $('#zoom-out-btn').click(function() {
                 MapController.Controls.zoomOutMap();
+            })
+            
+            //Pan left event handler
+            $('#pan-left-btn').click(function() {
+                MapController.Controls.panLeft();
+            })
+            
+            //Pan Right event handler
+            $('#pan-right-btn').click(function() {
+                MapController.Controls.panRight();
+            })
+            
+            //Pan up event handler
+            $('#pan-up-btn').click(function() {
+                MapController.Controls.panUp();
+            })
+            
+             //Pan up event handler
+            $('#pan-down-btn').click(function() {
+                MapController.Controls.panDown();
             })
         }
         
