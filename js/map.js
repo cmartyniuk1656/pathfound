62f729fb6c3caadbb83df4bf410b69029f0f9ccc
Chris Martyniuk <cmartyniuk1@gmail.com>
2018-03-17 21:45:24 -0400

    Updated gameroom layout, mapcontroller, added grid with borders

--- js/map.js
+++ js/map.js
@@ -2,10 +2,19 @@
 var MapController = {
     
     //Keep track of all the map objects in the gameroom
-    "mapList": {},
+    'mapList': {},
     
-    "scale": 0,                                         //Used for zooming the map
-    "map": document.getElementById("map"),              //The map element in the DOM
+    'scale': 0,                                         //Used for zooming the map
+    
+    "map": document.getElementById('map'),
+    "mapGrid": document.getElementById('map-grid'),
+
+    
+    "mapCellHeight": 21,                                    //TODO: Make user selectable
+    "mapCellWidth": 21,                                     //TODO: Make user selectable
+    
+    
+    "backgroundImg": '', 
     
     
     //Utility functions for map controls
@@ -15,14 +24,14 @@ var MapController = {
         "zoomInMap": function() {
             console.info(MapController.scale);
             MapController.scale++;
-            MapController.map.style.transform = "scale(1."+ MapController.scale +")";
+            MapController.map.style.transform = 'scale(1.'+ MapController.scale +')';
         },
         
         //Zoom out the map
         "zoomOutMap": function() {
             console.info(MapController.scale);
             MapController.scale--;
-            MapController.map.style.transform = "scale(1."+ MapController.scale +")";
+            MapController.map.style.transform = 'scale(1.'+ MapController.scale +')';
         }   
     },
     
@@ -32,24 +41,71 @@ var MapController = {
         "addAll": function() {
             
             //Zoom in event handler
-            $("#zoom-in-btn").click(function() {
+            $('#zoom-in-btn').click(function() {
                 MapController.Controls.zoomInMap();
             })
             
             //Zoom out event handler
-            $("#zoom-out-btn").click(function() {
+            $('#zoom-out-btn').click(function() {
                 MapController.Controls.zoomOutMap();
             })
         }
         
     },
     
+    //Utility functions
     "Util": {
         
+        "getMapBackground": function() {
+            MapController.backgroundImg = 'url("' + Temp.returnMapBackground() + '")';  //TODO: replace shunt function
+            console.info('BG image string:');
+            console.info(MapController.backgroundImg);
+        },
+        
+        "setMapBackground": function() {
+            
+            MapController.map.style.backgroundImage = MapController.backgroundImg;
+            
+        },
+        
+        "buildMapGrid": function() {
+            
+            var divString = '<div class="map-cell"></div>';
+            var cellCount = MapController.mapCellWidth * MapController.mapCellHeight;
+
+            for (i = 0; i < cellCount; i++) {
+                
+                MapController.map.innerHTML += divString;
+        
+            }
+        
+        },
+        
+        "setMapSize": function() {
+            
+            var height = (MapController.mapCellHeight * 50).toString() + 'px';
+            var width = (MapController.mapCellWidth * 50).toString() + 'px';
+            MapController.map.style.height = height;
+            MapController.map.style.width = width;
+        }
+        
+        
+    },
+    
+    "Init": function() {
+        MapController.Events.addAll();
+        MapController.Util.getMapBackground();
+        MapController.Util.setMapBackground();
+        MapController.Util.buildMapGrid();
+        MapController.Util.setMapSize();
     }
 }
 
 $(document).ready(function() {
     
     MapController.Events.addAll();
+        MapController.Util.getMapBackground();
+        MapController.Util.setMapBackground();
+        MapController.Util.buildMapGrid();
+        MapController.Util.setMapSize();
 })
---
+++ js/temp.js
@@ -0,0 +1,8 @@
+var Temp = {
+    
+    //Returns the path to an image for testing map functionality. 
+    "returnMapBackground": function() {
+        return "assets/img-map-placeholder.jpg";
+    }
+    
+}
